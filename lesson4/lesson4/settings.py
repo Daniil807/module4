c36dd085815f9eaf53f6bd1dd6c13ac3f2cf0fb2
Daniil807 <dmesecko28@gmail.com>
2023-07-09 17:15:07 +0400

    Добавил приложение app_lesson_4
    Приложение из урока: app_lesson4
    приложение к дз: app_lesson_4
    для перехода к app_lesson_4 перейдите по адресу http://127.0.0.1:8000/lesson_4

--- lesson4/lesson4/settings.py
+++ lesson4/lesson4/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'app_lesson4'
+    'app_lesson4',
+    'app_lesson_4'
 ]
 
 MIDDLEWARE = [
