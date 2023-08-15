975685a13d8f4e4c4e516a5641e3853adf2ac880
Daniil807 <dmesecko28@gmail.com>
2023-08-16 01:04:05 +0400

    Я ПОЧИНИЛ САЙТ
    ТАНЦУЕМ ВСЕМ ДЕВЕЛОПЕРСКИМ ПОДВАЛОМ

--- lesson4/app_lesson4/views.py
+++ lesson4/app_lesson4/views.py
@@ -12,16 +12,15 @@ def top_sellers(request):
     return render(request, 'top-sellers.html')
 
 def advertisement_post(request):
-    #form = AdvertisementForm()
-    #context = {'form' : form}
-    #return render(request, 'advertisement_post.html', context)
     if request.method == 'POST':
         form = AdvertisementForm(request.POST, request.FILES)
         if form.is_valid():
-            asvertisement = Advertisement(**form.cleaned_data)
-            Advertisement.user = request.user
-            Advertisement.save()
+            advertisement = Advertisement(**form.cleaned_data)
+            advertisement.user = request.user
+            advertisement.save()
             url = reverse('main-page')
             return redirect(url)
-        context = {'form':form}
-        return render(request, 'advertisement-post.html', context)
+    else:
+        form = AdvertisementForm()
+    context = {'form' : form}
+    return render(request, 'advertisement_post.html', context)
